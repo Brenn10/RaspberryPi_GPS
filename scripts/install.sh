c70c47042d54517c1c2d9203fce55431cca30d7a
Brennan Cain <brenn.cain@gmail.com>
2021-12-07 16:16:21 -0500

    working with install

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,5 @@ sudo cp roslaunch.service /etc/systemd/system/roslaunch.service
 sudo cp start_gps_daemon /usr/sbin/start_gps_daemon
 
 sudo systemctl enable roslaunch.service
+sudo systemctl restart roslaunch.service
 sudo chmod +x /usr/sbin/start_gps_daemon
